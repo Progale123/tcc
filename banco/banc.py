bcc4b25402707b682f2594a455b7440fa4dc7ff4
GUILHERME FERREIRA ALVES <00001084594705sp@aluno.educacao.sp.gov.br>
2026-08-19 11:02:13 -0300

    Criação do banco de dados para login de usuariso e correição de bugs em multiplas interfaces

--- banco/banc.py
+++ banco/banc.py
@@ -1,20 +1,68 @@
 import sqlite3
 import hashlib
 
-conexao = sqlite3.connect(r"C:\Users\GUILHERMEFERREIRAALV\tcc\banco\banco.db")
+def hash_senha(senha):
+    return hashlib.sha256(senha.encode()).hexdigest()
+
 def criar_banco():
-    conexao = sqlite3.connect("sistema_login.db")
+    conexao = sqlite3.connect("sistema_criarconta.db")
     cursor = conexao.cursor()
-    
-    # Estrutura com cargo e campos de usuário
+
     cursor.execute("""
-    CREATE TABLE IF NOT EXISTS usuarios (
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        nome TEXT NOT NULL,
-        email TEXT UNIQUE NOT NULL,
-        senha_hash TEXT NOT NULL,
-        cargo TEXT DEFAULT 'usuario'
-    )
+        CREATE TABLE IF NOT EXISTS usuarios (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            nome TEXT NOT NULL,
+            email TEXT UNIQUE NOT NULL,
+            senha_hash TEXT NOT NULL,
+            cargo TEXT DEFAULT 'usuario'
+        )
     """)
+
     conexao.commit()
     conexao.close()
+
+def cadastrar_usuario(nome, email, senha, cargo="usuario"):
+    conexao = sqlite3.connect("sistema_login.db")
+    cursor = conexao.cursor()
+
+    try:
+        cursor.execute("""
+            INSERT INTO usuarios (nome, email, senha_hash, cargo)
+            VALUES (?, ?, ?, ?)
+        """, (nome, email, hash_senha(senha), cargo))
+        conexao.commit()  
+        print("Usuário cadastrado com sucesso!")
+    except sqlite3.IntegrityError:
+        print("Erro: email já cadastrado.")
+    finally:
+        conexao.close()
+
+criar_banco()
+cadastrar_usuario("guilherme", "teste123@gmail.com", "senha", "admin")
+cadastrar_usuario("João", "joao@email.com", "123456", "estagiario")
+cadastrar_usuario("Maria", "maria@email.com", "abc123", "admin")
+
+
+def informa_dados():
+     conexao = sqlite3.connect("sistema_login.db")
+     cursor = conexao.cursor()
+
+     nome = input("digite seu nome:")
+     if nome !="" :
+      print("Campo preenchido, avançando...")
+     else:
+      print("Campo vazio")
+
+     email = input("Digite seu email:")  
+     if email !="" :
+      print("Campo preenchido, avançando")
+     else:
+      print("Campo vazio")
+
+     senha = input("Crie uma senha")
+     if senha !="" :
+      print("Campo preenchido, avançando")
+     else:
+      print("Campo vazio") 
+     
+      conexao.close()
